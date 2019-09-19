c2f6565d525cb1162df79ff44ef8b20de1054d59
Alyaa AlOstad <alyaaalostad@hotmail.com>
2019-09-19 11:45:31 +0300

    done

--- src/App.js
+++ src/App.js
@@ -8,13 +8,30 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    bold: false,
+    italic: false,
+    underline: false,
+    color: ""
+  };
+
+  setColor(color) {
+    this.setState({ color: color });
+  }
+  setStyle(font) {
+    this.setState({ [font]: !this.state[font] });
+  }
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => this.setStyle(style)}
+        >
           {style}
         </button>
       );
@@ -25,6 +42,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.setColor(color)}
         />
       );
     });
@@ -32,7 +50,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.color,
+            fontStyle: this.state.italic ? "italic" : "",
+            fontWeight: this.state.bold ? "bold" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
